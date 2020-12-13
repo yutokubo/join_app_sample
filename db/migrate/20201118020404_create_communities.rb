28ea3aad7c7b1667604bdb5beb58307858c2c768
KUBO1789 <jackbook1750@gmail.com>
2020-12-13 19:51:43 +0900

    add change

--- db/migrate/20201118020404_create_communities.rb
+++ db/migrate/20201118020404_create_communities.rb
@@ -1,7 +1,7 @@
 class CreateCommunities < ActiveRecord::Migration[6.0]
   def change
     create_table :communities do |t|
-    　t.index　     :user_id
+    　t.references　 :user
       t.string     :title
       t.string     :intro_image_id
       t.text       :introduction
--- db/migrate/20201127125227_rename_community_name_column_to_communities.rb
+++
@@ -1,5 +0,0 @@
-class RenameCommunityNameColumnToCommunities < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :communities, :community_name, :title
-  end
-end
